7603a09206d0429c61f6554e38b520d3030a4740
therewasaguy <jason.sigal@gmail.com>
2016-09-21 18:16:43 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next){
+  console.log('your name is ' + req.query.name);
+  next();
+});
+
+// a random route
+app.get('/random', function(req, res){
+  res.send(String(Math.random()));
+});
+
+app.get('/profile/:name', function(req, res) {
+  res.status(200);
+  res.type('json');
+  res.send({
+    name: req.params.name
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next){
+  res.status(404);
+  res.send('404 - page not found');
+});
+
+
+app.listen(3001);
+
+console.log('listening on port 3001');
